f1bb1859274623fa78993ddceb7512d3510a2785
alba-ta <alba.tallon.espinosa@gmail.com>
2025-10-02 18:54:54 +0200

    Update dbtest.js

--- simple-glpi-Api/dbtest.js
+++ simple-glpi-Api/dbtest.js
@@ -1,19 +1,1 @@
-import mysql from 'mysql2';
 
-// create the connection
-const conn = mysql.createConnection({
-  host: 'localhost', // dirección del servidor MySQL
-  user: 'root', // usuario de MySQL
-  password: '1234', // la contraseña de MySQL
-  database: 'glpi',
-  port: 3306 // puerto de MySQL (por defecto es 3306)
-});
-
-conn
-  .promise()
-  .query('SELECT 1')
-  .then(([rows, fields]) => {
-    console.log(rows);
-  })
-  .catch(console.log)
-  .then(() => conn.end());
